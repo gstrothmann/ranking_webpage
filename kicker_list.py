92bff8e3b3eb7da99c54ad084e8fcc8b3e0d5ee1
Gideon Strothmann <gideon.strothmann@vattenfall.com>
2019-10-02 09:47:09 +0200

    very rough structure, navbar, a register form that is not working yet

---
+++ kicker_list.py
@@ -0,0 +1,38 @@
+from flask import Flask, escape, request, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+from get_data import get_data
+import json, plotly
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '0798aaa973a4f300d8a2277a9db963f2'
+
+passed_data = get_data()
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    name = request.args.get("name", "World")
+    return render_template('home.html', data = passed_data)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', data = passed_data)
+
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = 'Register', form = form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form = form)
+
+if __name__ == '__main__':
+    app.run(debug = True)
